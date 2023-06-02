409d6605cff8409e79e3a176ea85908aa51d1684
holger <holger@hfischer.org>
2023-06-02 14:07:55 +0200

    add cors

---
+++ mfcomposite/src/main/java/de/hf/myfinance/composite/CorsGlobalConfiguration.java
@@ -0,0 +1,17 @@
+package de.hf.myfinance.composite;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class CorsGlobalConfiguration implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")          
+        .allowedOrigins("*");
+    }
+}
