fc38c5d071e85dd6a04a9efef70f54d9b12fffd8
holgerfischer <holger@hfischer.org>
2024-08-16 11:06:12 +0200

    add auditevent for recurrenttransactions

--- mfframework/src/main/java/de/hf/framework/audit/AuditType.java
+++ mfframework/src/main/java/de/hf/framework/audit/AuditType.java
@@ -4,6 +4,7 @@ public enum AuditType {
     USERINFO, // informations for the user like instrument saved
     INSTRUMENTEVENT,    // A Pushevent via Websocket to trigger Frontendupdates
     TRANSACTIONEVENT,   // A Pushevent via Websocket to trigger Frontendupdates
+    RECURRENTTRANSACTIONEVENT,   // A Pushevent via Websocket to trigger Frontendupdates
     ERROR,  // A business error
     FATAL, // A fatal error in the Backend e.G:server crash
     DEBUG, //not published via Websocket only logged to the console
