6046cbdba96070ca22a656145c03e66e0ba8957d
holgerfischer <holger@hfischer.org>
2025-02-07 14:18:02 +0100

    add instrument properties

--- mfapi/src/main/java/de/hf/myfinance/restmodel/AdditionalProperties.java
+++ mfapi/src/main/java/de/hf/myfinance/restmodel/AdditionalProperties.java
@@ -1,10 +1,17 @@
 package de.hf.myfinance.restmodel;
 
+import java.lang.ref.Reference;
+
 public enum AdditionalProperties {
     DEFAULTGIROID,
     INCOMEBUDGETID,
     REALESTATEBUDGETGROUPID,
     MATURITYDATE,
+    ACQUISITIONDATE,
+    ACQUISITIONVALUE,
+    INTERESTRATE,
+    ANNUITYRATE,
+    REFERENCEGIRO,
     CURRENCYCODE,
     VALUEBUDGETID,
     ISIN,
