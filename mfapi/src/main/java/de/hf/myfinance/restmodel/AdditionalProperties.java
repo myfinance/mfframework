d1ad56f0d6e03161b3721afff6b0d8dff0c10995
holgerfischer <holger@hfischer.org>
2025-02-10 13:10:28 +0100

    add instrument property

--- mfapi/src/main/java/de/hf/myfinance/restmodel/AdditionalProperties.java
+++ mfapi/src/main/java/de/hf/myfinance/restmodel/AdditionalProperties.java
@@ -1,11 +1,11 @@
 package de.hf.myfinance.restmodel;
 
-import java.lang.ref.Reference;
 
 public enum AdditionalProperties {
     DEFAULTGIROID,
     INCOMEBUDGETID,
     REALESTATEBUDGETGROUPID,
+    LINKEDINSTRUMENTID, // i.G. the linked RealEstate for a BudgetGroup
     MATURITYDATE,
     ACQUISITIONDATE,
     ACQUISITIONVALUE,
