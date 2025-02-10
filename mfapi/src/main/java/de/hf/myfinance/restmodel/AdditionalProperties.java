85535b025908e06ecf6f6bc7dd2205cb9ad09875
holgerfischer <holger@hfischer.org>
2025-02-10 17:58:37 +0100

    remove property realestateID

--- mfapi/src/main/java/de/hf/myfinance/restmodel/AdditionalProperties.java
+++ mfapi/src/main/java/de/hf/myfinance/restmodel/AdditionalProperties.java
@@ -4,7 +4,6 @@ package de.hf.myfinance.restmodel;
 public enum AdditionalProperties {
     DEFAULTGIROID,
     INCOMEBUDGETID,
-    REALESTATEBUDGETGROUPID,
     LINKEDINSTRUMENTID, // i.G. the linked RealEstate for a BudgetGroup
     MATURITYDATE,
     ACQUISITIONDATE,
